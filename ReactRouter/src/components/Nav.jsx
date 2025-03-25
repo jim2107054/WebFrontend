c894d2dbc86dc20204b9322a219d31f78cbd72fe
jim2107054 <mdjahid205hasan@gmail.com>
2025-03-25 13:00:31 +0600

    react router dom

---
+++ ReactRouter/src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../App.css"
+
+const Nav = () => {
+  return (
+    <nav>
+        <h2>Vite Logo</h2>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
